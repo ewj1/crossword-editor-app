41c778bf8ce7b71beb7f4dd659fefa8470467daa
Ezra Joffe-Hancock <ewj1@williams.edu>
2025-09-28 09:05:55 -0600

    fix editable title auto highlighting bug

--- src/components/EditableSpan.jsx
+++ src/components/EditableSpan.jsx
@@ -1,14 +1,24 @@
-export function EditableSpan({ value, setValue }) {
+import { useRef } from "react";
+import { useEditable } from "use-editable";
+
+export function EditableSpan({ value, setValue, styleVariant = "" }) {
+  const spanRef = useRef(null);
+  useEditable(spanRef, setValue);
   return (
-    <label className="relative inline-block border-b-2 border-white focus-within:border-blue-500">
-      <span className="whitespace-pre">{value}</span>
-      <input
-        className="absolute top-0 left-0 w-full bg-white align-top outline-none"
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-      ></input>
-    </label>
+    <span
+      className={`border-b-2 border-white outline-none focus:border-blue-500 ${styleVariant}`}
+      ref={spanRef}
+    >
+      {value}
+    </span>
   );
 }
+//     <label className="relative inline-block border-b-2 border-white focus-within:border-blue-500"></label>
+// <input
+//         className="absolute top-0 left-0 bg-white align-top outline-none"
+//         style={{ width: "100%" }}
+//         value={value}
+//         onChange={(e) => {
+//           setValue(e.target.value);
+//         }}
+//       ></input>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,10 +2,16 @@ import { EditableSpan } from "./EditableSpan";
 
 export function Title({ title, setTitle, name, setName }) {
   return (
-    <div>
-      <EditableSpan value={title} setValue={setTitle}></EditableSpan>
-      <span className="mx-2">by</span>
-      <EditableSpan value={name} setValue={setName}></EditableSpan>
-    </div>
+    <>
+      <div className="m-2 flex text-2xl text-gray-400">
+        <EditableSpan
+          value={title}
+          setValue={setTitle}
+          styleVariant="text-black"
+        ></EditableSpan>
+        <span className="mx-[0.5rem]">by</span>
+        <EditableSpan value={name} setValue={setName}></EditableSpan>
+      </div>
+    </>
   );
 }
