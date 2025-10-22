443956c67f8edb16c94408cf9eae42283a77a226
Ezra Joffe-Hancock <ewj1@williams.edu>
2025-10-21 22:03:28 -0600

    remove port listening

--- backend/index.js
+++ backend/index.js
@@ -53,7 +53,3 @@ app.get(/.*/, (req, res) => {
     return res.status(404).json({ error: "Not found" });
   res.sendFile(path.join(staticPath, "index.html"));
 });
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
