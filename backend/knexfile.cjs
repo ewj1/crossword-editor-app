dcd4fc41216c71e0984db043f180d8ad7240f6dd
Ezra Joffe-Hancock <ewj1@williams.edu>
2025-10-22 12:14:57 -0600

    adding different knex environments

--- backend/knexfile.cjs
+++ backend/knexfile.cjs
@@ -1,17 +1,20 @@
 require("./config/loadEnv.cjs");
 
-module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
-    },
-    migrations: {
-      directory: "./migrations",
-    },
+const config = {
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
   },
+  migrations: {
+    directory: "./migrations",
+  },
+};
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
 };
